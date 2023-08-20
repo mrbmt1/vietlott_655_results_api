ecee09af5d073c361c39b8113034b27b34d61ca2
Minh Thuong <mrbmt1@gmail.com>
2023-08-20 22:22:42 +0700

    second

--- script.js
+++ script.js
@@ -1,19 +1,10 @@
 document.addEventListener("DOMContentLoaded", async () => {
     const response = await fetch("vietlott_result_api.txt");
     const data = await response.json();
+    
     const resultsDiv = document.getElementById("results");
-
     data.forEach(result => {
-        const resultElement = document.createElement("div");
-        resultElement.classList.add("result");
-
-        const drawPeriod = document.createElement("p");
-        drawPeriod.textContent = `Draw Period: ${result.draw_period}`;
-
-        // Thêm các thuộc tính khác tương tự ở đây
-
-        resultElement.appendChild(drawPeriod);
-
-        resultsDiv.appendChild(resultElement);
+        const resultText = JSON.stringify(result);
+        resultsDiv.innerHTML += `<p>${resultText}</p>`;
     });
 });
